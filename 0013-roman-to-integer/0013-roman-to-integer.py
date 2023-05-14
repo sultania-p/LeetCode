b4627733b6a807a2c7e9e0542a3721a36ee2fe58
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-14 01:37:58 -0400

    Time: 56 ms (45.58%), Space: 16.3 MB (28.15%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # create a hashmap
+        set = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        i = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and set[s[i]] < set[s[i+1]]:
+                total += set[s[i]] * -1
+                # print(total)
+            else:
+                total += set[s[i]]
+                # print(total)
+            i += 1
+
+        return total

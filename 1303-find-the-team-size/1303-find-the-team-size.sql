44d9df20dcf64b915221a912d635448b47fbeb94
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-19 18:22:09 -0400

    Time: 409 ms (73.99%), Space: 0B (100.00%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,12 +1,13 @@
 /* Write your T-SQL query statement below */
-select 
-	employee_id,
-	sq.team_size
-from employee e 
-left join (
+with cte as (
 			select
 				team_id,
 				count(team_id) as team_size
 			from Employee
 			group by team_id
-		) sq on e.team_id = sq.team_id
+)
+select 
+	employee_id,
+	sq.team_size
+from employee e 
+left join cte sq on e.team_id = sq.team_id

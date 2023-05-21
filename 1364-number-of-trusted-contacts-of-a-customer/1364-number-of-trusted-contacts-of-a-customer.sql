967c3c37d785da3b08d41bcb5e9ce349a1ef8164
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-20 20:15:10 -0400

    Time: 2275 ms (84.62%), Space: 0B (100.00%) - LeetHub

--- 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
+++ 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
@@ -1,4 +1,14 @@
 /* Write your T-SQL query statement below */
+with cte_contact as (
+	select  user_id, count(contact_name) as contacts_cnt 
+				from Contacts Ct 
+				group by user_id
+), cte_trusted_contact as (
+	select  user_id, count(contact_name) as trusted_contacts_cnt 
+				from Contacts Ct 
+				where contact_email in (select email from Customers)
+				group by user_id
+)
 select
 	I.invoice_id,
 	C.customer_name,
@@ -7,13 +17,6 @@ select
 	isnull(sq1.trusted_contacts_cnt, 0) as trusted_contacts_cnt
 from Invoices I
 left join Customers C on I.user_id=C.customer_id
-left join (select  user_id, count(contact_name) as contacts_cnt 
-				from Contacts Ct 
-				group by user_id
-		) sq on I.user_id=sq.user_id
-left join (select  user_id, count(contact_name) as trusted_contacts_cnt 
-				from Contacts Ct 
-				where contact_email in (select email from Customers)
-				group by user_id
-			) sq1 on I.user_id=sq1.user_id
+left join cte_contact sq on I.user_id=sq.user_id
+left join cte_trusted_contact sq1 on I.user_id=sq1.user_id
 order by I.invoice_id

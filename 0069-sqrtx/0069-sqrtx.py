c588882c5ff0c9652b428f34f5e974134b5db79f
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-23 03:21:45 -0400

    Time: 5350 ms (5.01%), Space: 16.4 MB (5.40%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,9 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        if x == 1:
-            return 1
+        if x < 2:
+            return x
 
         for i in range(0, x):
             if i * i == x:

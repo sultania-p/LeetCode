2e2ba6ed5d3e7efb9c4b612591c06f7658c19362
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-28 19:09:56 -0400

    Time: 2160 ms (67.94%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,6 @@
 /* Write your T-SQL query statement below */
 with cte as (
 select
-	V.visit_id,
 	customer_id,
 	transaction_id
 from Visits V
@@ -11,4 +10,3 @@ select customer_id, count(1) as count_no_trans
 from cte 
 where transaction_id is null
 group by customer_id
-order by 2 desc

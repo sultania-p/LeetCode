fa70ede3fd3ff40b6801f7fdc1a70132c0ccd6d7
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-28 20:31:41 -0400

    Time: 2547 ms (86.75%), Space: 0B (100.00%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -7,17 +7,17 @@ with cte1 as (
 	from Orders O
 	group by customer_id, product_id
 ), cte2 as (
-	select customer_id,
-			product_id,
-			prod_order_count,
-			DENSE_RANK() over (partition by  customer_id order by prod_order_count desc) as rn
+	select cte1.customer_id,
+			cte1.product_id,
+			P.product_name,
+			DENSE_RANK() over (partition by  cte1.customer_id order by prod_order_count desc) as rn
 	from cte1
+	left join products P on cte1.product_id=P.product_id
+	left join Customers C on cte1.customer_id=C.customer_id
 )
 select 
-	cte2.customer_id,
-	cte2.product_id,
-	P.product_name
+	customer_id,
+	product_id,
+	product_name
 from cte2
-left join products P on cte2.product_id=P.product_id
-left join Customers C on cte2.customer_id=C.customer_id
 where rn = 1

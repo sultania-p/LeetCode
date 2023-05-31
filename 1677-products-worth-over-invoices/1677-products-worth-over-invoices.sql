2a064a27b294500eecc49aa81074917d3e0c0cce
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-05-31 18:54:20 -0400

    Time: 1134 ms (6.90%), Space: 0B (100.00%) - LeetHub

--- 1677-products-worth-over-invoices/1677-products-worth-over-invoices.sql
+++ 1677-products-worth-over-invoices/1677-products-worth-over-invoices.sql
@@ -1,10 +1,11 @@
 /* Write your T-SQL query statement below */
+
 select
 	p.name,
-	sum(isnull(rest, 0)) as rest,
-	sum(isnull(paid, 0)) as paid,
-	sum(isnull(canceled, 0)) as canceled,
-	sum(isnull(refunded, 0)) as refunded
+	isnull(sum(rest), 0) as rest,
+	isnull(sum(paid), 0) as paid,
+	isnull(sum(canceled), 0) as canceled,
+	isnull(sum(refunded), 0) as refunded
 from Product P
 left join Invoice I  on I.product_id=P.product_id
 group by p.name

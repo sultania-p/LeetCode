38e37ceb006c11f36415d1222e1a15152d7fc084
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-06-03 18:37:13 -0400

    Time: 1109 ms (90.82%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,20 +1,12 @@
 /* Write your T-SQL query statement below */
-with cte as (
+
 	select
-		E.employee_id as  employee_id,
-		E.name as employee_name,
-		E.age as employee_age,
-		M.employee_id as manager_id,
-		M.name as manager_name
+		M.employee_id,
+		M.name,
+		count(1) as reports_count,
+		cast(ROUND(avg(E.age * 1.00), 0) as int) as average_age 
 	from Employees E
 	left join Employees M on M.employee_id = E.reports_to
-)
-select 
-	manager_id as employee_id,
-	manager_name as name,
-	count(employee_id) as reports_count,
-	cast(ROUND(avg(employee_age * 1.00), 0) as int) as average_age 
-from cte
-where manager_id is not null
-group by manager_id, manager_name
-order by manager_id
+	where M.employee_id is not null
+	group by M.employee_id, M.name
+	order by M.employee_id

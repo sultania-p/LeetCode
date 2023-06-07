51a7574c74ca1abc8b7e48ecf459fb60ffdcca79
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-06-07 17:40:39 -0400

    Time: 749 ms (64.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,13 @@
+/* Write your T-SQL query statement below */
+select
+	employee_id,
+	department_id
+from Employee
+where primary_flag='Y'
+UNION
+select
+	employee_id,
+	max(department_id)
+from Employee
+group by employee_id
+having count(department_id) = 1

a3a9bd92adf55857c81edfe54fa1c72125f5e757
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-06-09 12:38:38 -0400

    Time: 784 ms (72.83%), Space: 0B (100.00%) - LeetHub

--- 1821-find-customers-with-positive-revenue-this-year/1821-find-customers-with-positive-revenue-this-year.sql
+++ 1821-find-customers-with-positive-revenue-this-year/1821-find-customers-with-positive-revenue-this-year.sql
@@ -1,6 +1,9 @@
 /* Write your T-SQL query statement below */
+with cte as (
 select
-	customer_id
+	customer_id,
+	revenue
 from Customers
 where year= 2021
-and revenue >0
+)
+select customer_id from cte where revenue > 0

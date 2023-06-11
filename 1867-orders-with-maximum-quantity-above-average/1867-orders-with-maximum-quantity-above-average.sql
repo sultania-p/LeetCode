680589798a72078c0c6898a0a5fc057f58e53c9e
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2023-06-11 17:35:36 -0400

    Time: 1632 ms (30.56%), Space: 0B (100.00%) - LeetHub

--- 1867-orders-with-maximum-quantity-above-average/1867-orders-with-maximum-quantity-above-average.sql
+++ 1867-orders-with-maximum-quantity-above-average/1867-orders-with-maximum-quantity-above-average.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
+/*
 with cte_avg as (
 	select
 		order_id,
@@ -14,3 +15,15 @@ cross join cte_avg c2
 where c1.max_quantity > c2.avg_quantity
 group by c1.order_id
 having count(c1.order_id) = (select count(distinct order_id) from OrdersDetails)
+*/
+
+-- Sol - 2 
+with cte_avg as (
+	select
+		sum(quantity) * 1.00 / count(distinct product_id) as avg_quantity
+	from OrdersDetails
+	group by order_id
+)
+select distinct c1.order_id
+from OrdersDetails c1
+where c1.quantity > (select max(avg_quantity) from cte_avg)
